475503ce62d63f9e7d02b844900f57ceb2604f55
Anaïs <anaisamato81@gmail.com>
2017-04-07 18:07:09 +0200

    Modif formulaire stats

--- Javascript/formStats.js
+++ Javascript/formStats.js
@@ -1,31 +1,102 @@
 $(document).ready(function() {
   
+  	var lien = 0;
+	// complete le formulaire si on souhaite faire un graphique du nombre de manifs manifs selon les quartiers
+   	$("#lienManifs").click(function() {
+   		var field  = "<fieldset>"
+				+ "<legend><strong>Quartiers</strong></legend>"
+				+ "<label>Sélectionnez les quartiers que vous souhaitez comparer : </label>"
+				+ "<select id='selectQuartierStat'>"
+				+	"<option value='0'>Sélection de quartier</option>"
+				+	"<option value='1'>Quartier 1</option>"
+				+	"<option value='2'>Quartier 2</option>"
+				+	"<option value='3'>Quartier 3</option>"
+				+	"<option value='4'>Tous les quartiers</option>"
+				+ "</select>"
+				+ "<div id='selection'></div>"
+				+ "</fieldset>";
+		if(lien == 0) {
+			$("#manifs").append(field);
+			lien = 1;
+		}
+   	});
 
-    var selectQuartierStat = $("#selectQuartierStat");
 
-	
-   	$(selectQuartierStat).change(function () {
-   		if(selectQuartierStat.val() != 0) {
-   			if(selectQuartierStat.val() ==4 ) {
+   	// complete le formulaire si on souhaite faire un graphique du nombre de salles selon les quartiers
+   	$("#lienSalles").click(function() {
+
+   		var field  = "<fieldset>"
+				+ "<legend><strong>Quartiers</strong></legend>"
+				+ "<label>Sélectionnez les quartiers que vous souhaitez comparer : </label>"
+				+ "<select id='selectQuartierStat'>"
+				+	"<option value='0'>Sélection de quartier</option>"
+				+	"<option value='1'>Quartier 1</option>"
+				+	"<option value='2'>Quartier 2</option>"
+				+	"<option value='3'>Quartier 3</option>"
+				+	"<option value='4'>Tous les quartiers</option>"
+				+ "</select>"
+				+ "<div id='selection'></div>"
+				+ "</fieldset>";
+		if(lien == 0) {
+			$("#salles").append(field);
+			lien = 1;
+		}
+   	});
+
+
+   	// création des tags de quartiers
+   	var selectQuartierStat = $("#selectQuartierStat");
+   	$("body").on("change", "#selectQuartierStat", function() {
+   		var champ = $("#selectQuartierStat option:selected").text();
+    	var id = $("#selectQuartierStat option:selected").val();
+
+   		if(id != 0) {
+   			if(id ==4 ) {
     			$("#selection div").each(function() {
     				$(this).remove();
     			});
     		}
-    		var champ = $("#selectQuartierStat option:selected").text();
+    		
     		console.log(champ);
-    		var newDiv = "<div id='"+selectQuartierStat.val()+"'></div>";
+    		console.log(id);
+    		var newDiv = "<div id='"+id+"'></div>";
     		$("#selection").append(newDiv);
     		var contenu = "<p>"+champ+"</p>";
     		var croix = "<img src='Images/croix.png' class='test'width='8px' height='8px'>";
-    		$("#"+selectQuartierStat.val()).append(contenu,croix);
+    		$("#"+id).append(contenu,croix);
 
-    		
     	}
-
-
   	});
-   	$("body").on('click', '.test', function() { 
+  	
+  	$("body").on('click', '.test', function() { 
   		$(this).parent().remove();
   	});
 
+   	// Rajout stats aléatoires trouvé sur internet pour tester l'affichage de graphique
+  	google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('placeStats'));
+
+        chart.draw(data, options);
+    }
+
+
 });
+
+
